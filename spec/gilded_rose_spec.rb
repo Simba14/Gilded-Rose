5c8009ebc734e874277a8244447189e45d8e7f9e
Simba14 <seanblundell@gmail.com>
2017-05-16 11:16:51 +0100

    Test: normal good - sell-in value falls by one

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,12 +1,29 @@
 require 'gilded_rose'
+require 'item'
 
 describe GildedRose do
 
+  def item_list(name: 'normal', sell_in: 1, quality: 50)
+    @item = Item.new(name, sell_in, quality)
+    @items = [@item]
+  end
+
   describe '#update_quality' do
-    it 'does not change the name' do
-      items = [Item.new('foo', 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq 'fixme'
+    context 'item is a normal good' do
+      before do
+        item_list
+        @stock = described_class.new(@items)
+      end
+
+      it 'name does not change' do
+        @stock.update_quality
+        expect(@item.name).to eq 'normal'
+      end
+
+      it 'items sell in value falls by one' do
+        @stock.update_quality
+        expect(@item.sell_in).to equal 0
+      end
     end
   end
 
