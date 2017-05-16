ae26201a5c90a1eba3e117e3816b2226a45155cf
Simba14 <seanblundell@gmail.com>
2017-05-16 14:14:46 +0100

    Test: Backstage goods - quality increase dependent on sell in date

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -9,20 +9,32 @@ describe GildedRose do
   end
 
   describe '#update_quality' do
-
     context 'all goods' do
       before do
         item_list(quality: 0)
         @stock = described_class.new(@items)
+        @stock.update_quality
+      end
+
+      it 'name does not change' do
+        expect(@item.name).to eq 'normal'
       end
 
       it 'the minimum quality value is 0' do
-        @stock.update_quality
         expect(@item.quality).to eq 0
       end
 
       it 'quality value can never be negative' do
         expect { @stock.update_quality }.not_to(change { @item.quality })
+      end
+
+      it 'sell in value falls by one' do
+        expect(@item.sell_in).to equal 0
+      end
+
+      it 'sell in value can be negative' do
+        @stock.update_quality
+        expect(@item.sell_in).to equal(-1)
       end
     end
 
@@ -33,23 +45,77 @@ describe GildedRose do
         @stock.update_quality
       end
 
-      it 'name does not change' do
-        expect(@item.name).to eq 'normal'
-      end
-
-      it 'sell in value falls by one' do
-        expect(@item.sell_in).to equal 0
-      end
-
-      it 'before sell by date is reached, quality value decreases by one' do
+      it 'before sell by date is reached, quality value decreases by 1' do
         expect(@item.quality).to equal 49
       end
 
-      it 'once sell by date is past, quality value decreases by double' do
+      it 'once sell by date is past, quality value falls by 2' do
         @stock.update_quality
         expect(@item.quality).to equal 47
       end
+    end
 
+    context "item is 'Aged Brie'" do
+      context 'before sell by date is reached' do
+        before do
+          item_list(name: 'Aged Brie', quality: 49)
+          @stock = described_class.new(@items)
+          @stock.update_quality
+        end
+
+        it 'quality value increases by 1 with time' do
+          expect(@item.quality).to eq 50
+        end
+
+        it 'quality value cannot exceed 50' do
+          expect { @stock.update_quality }.not_to(change { @item.quality })
+        end
+      end
+
+      context 'once sell by date is past' do
+        before { item_list(name: 'Aged Brie', quality: 10, sell_in: 0)}
+        it 'quality increases by 2 with time' do
+          @stock = described_class.new(@items)
+          @stock.update_quality
+          expect(@item.quality).to eq 12
+        end
+      end
+    end
+
+    context "item is 'Sulfuras, Hand of Ragnaros'" do
+      before do
+        item_list(name: 'Sulfuras, Hand of Ragnaros', quality: 80)
+        @stock = described_class.new(@items)
+      end
+
+      it 'sell in value does not change' do
+        expect { @stock.update_quality }.not_to(change { @item.sell_in })
+      end
+
+      it 'quality value is constant at 80' do
+        @stock.update_quality
+        expect(@item.quality).to eq 80
+      end
+    end
+
+    context "item is 'Backstage passes to a TAFKAL80ETC concert'" do
+      context 'before sell by date is reached' do
+        before do
+          item_list(name: 'Backstage passes to a TAFKAL80ETC concert', quality: 40, sell_in: 10)
+          @stock = described_class.new(@items)
+        end
+
+        it 'quality value increase by 2 once sell in date is lower than 11 days' do
+          @stock.update_quality
+          expect(@item.quality).to eq 42
+        end
+
+        it 'quality value increase by 3 once sell in date is lower than 6 days' do
+          @item.sell_in = 5
+          @stock.update_quality
+          expect(@item.quality).to eq 43
+        end
+      end
     end
   end
 
